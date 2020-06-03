ed480b683e1e97cf6fb97fcc8adfbb6f5716bd2e
Edgar Giovanni Hurtarte <geohurtarte2015@users.noreply.github.com>
2020-06-03 03:17:44 -0600

    test2

--- projectcripto/src/main/java/com/renovaus/Test2.java
+++ projectcripto/src/main/java/com/renovaus/Test2.java
@@ -1,5 +1,15 @@
 package com.renovaus;
 
 public class Test2 {
+	
+	private int numerosDos;
+
+	public int getNumerosDos() {
+		return numerosDos;
+	}
+
+	public void setNumerosDos(int numerosDos) {
+		this.numerosDos = numerosDos;
+	}
 
 }
